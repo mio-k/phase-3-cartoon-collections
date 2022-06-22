e86da62cd88ec6e7db2359a72c6ec94c75694f26
mio kataoka <mio.kanna@gmail.com>
2022-06-22 13:52:21 -0700

    commit tree 6eddcfedab71f2f54edc3f94588cb202773be8a3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+test_dwarves = ["Dray", "Axel", "Mugi", "Clover"]
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, i|
+  puts "#{i}. #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+roll_call_dwarves(test_dwarves)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+test_calls = ["dray", "axel", "mugi", "clover"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def summon_captain_planet(calls)
+  newArray = calls.map do |call|
+    call.capitalize + "!"
+  end 
+  newArray
+end
+summon_captain_planet(test_calls)
+
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
+end
+long_planeteer_calls(test_calls)
+
+itemArray = ["Draymond", "cheddar", "Mugi"]
+
+def find_the_cheese(strArr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strArr.find do |item|
+  cheese_types.include?(item)
+  end 
 end
+find_the_cheese(itemArray)
